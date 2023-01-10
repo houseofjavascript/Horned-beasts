4e74ff608b95e4c435c5f8cf15f7ae44095de945
‘Diontre <diontre01@gmail.com>
2023-01-09 20:39:53 -0600

    changes for horned beast

---
+++ src/footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class Footer extends React.Component{
+  render(){
+    return (
+    <>
+      <h1>Diontre Sanders</h1>
+    </>
+    )
+  }
+}
+
+export default Footer;
